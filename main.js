3de90614766636bc126f1655562035db2cd3d649
Diego <dg456398@gmail.com>
2025-02-04 19:29:18 -0300

    janela electron

---
+++ main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { exec } = require('child_process'); // Para rodar o comando do React
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL('http://localhost:3000');
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(() => {
+  exec('npm start', (err, stdout, stderr) => {
+    if (err) {
+      console.error('Erro ao iniciar o React:', err);
+      return;
+    }
+    console.log(stdout);
+    console.error(stderr);
+
+    createWindow();
+  });
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
